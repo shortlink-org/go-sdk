5f907cfec0970b4897697ba6d6086028c826782b
Viktor Login <batazor111@gmail.com>
2025-10-21 23:51:22 +0400

    observability: add default preset

--- observability/traicing/traicing.go
+++ observability/traicing/traicing.go
@@ -15,8 +15,35 @@ import (
 	"go.opentelemetry.io/otel/sdk/trace"
 
 	"github.com/shortlink-org/go-sdk/logger"
+
 	"github.com/shortlink-org/go-sdk/observability/common"
 )
+
+// New returns a new instance of the TracerProvider.
+//
+//nolint:ireturn // It's make by specification
+func New(ctx context.Context, log logger.Logger) (*trace.TracerProvider, func(), error) {
+	viper.SetDefault("TRACER_URI", "localhost:4317")                     // Tracing addr:host
+	viper.SetDefault("PYROSCOPE_URI", "http://pyroscope.pyroscope:4040") // Pyroscope addr:host
+
+	config := Config{
+		ServiceName:    viper.GetString("SERVICE_NAME"),
+		ServiceVersion: viper.GetString("SERVICE_VERSION"),
+		URI:            viper.GetString("TRACER_URI"),
+		PyroscopeURI:   viper.GetString("PYROSCOPE_URI"),
+	}
+
+	tracer, tracerClose, err := Init(ctx, config, log)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if tracer == nil {
+		return nil, func() {}, nil
+	}
+
+	return tracer, tracerClose, nil
+}
 
 // Init returns an instance of Tracer Provider that samples 100% of traces and logs all spans to stdout.
 func Init(ctx context.Context, cnf Config, log logger.Logger) (*trace.TracerProvider, func(), error) {
