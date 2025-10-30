3edf3f57abc3093566169bf85e2840567defaefe
Viktor Login <batazor111@gmail.com>
2025-10-30 23:47:48 +0300

    concurrency: init

---
+++ concurrency/sync_map/sync_map_test.go
@@ -0,0 +1,19 @@
+package sync_map_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/shortlink-org/go-sdk/concurrency/sync_map"
+)
+
+func Test_SyncMap(t *testing.T) {
+	sm := sync_map.New()
+
+	for i := range 1000 {
+		sm.Set(i, "value")
+	}
+
+	require.Equal(t, "value", sm.Get(5))
+}
