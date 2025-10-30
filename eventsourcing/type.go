100d67207183f3ea104d16ea72fcf4deb1b78a98
Viktor Login <batazor111@gmail.com>
2025-10-30 23:55:14 +0300

    eventsourcing: init

---
+++ eventsourcing/type.go
@@ -0,0 +1,24 @@
+package eventsourcing
+
+import (
+	"context"
+
+	eventsourcing "github.com/shortlink-org/go-sdk/eventsourcing/domain/eventsourcing/v1"
+)
+
+// EventSourcing - interface for event store
+type EventSourcing interface {
+	Save(ctx context.Context, events []*eventsourcing.Event) error
+	SafeSave(ctx context.Context, events []*eventsourcing.Event) error
+	Load(ctx context.Context, aggregateID string) (*eventsourcing.Snapshot, []*eventsourcing.Event, error)
+
+	GetAggregateWithoutSnapshot(ctx context.Context) ([]*eventsourcing.BaseAggregate, error)
+	SaveSnapshot(ctx context.Context, snapshot *eventsourcing.Snapshot) error
+}
+
+// eventSourcing is the implementation of the event store
+type eventSourcing struct {
+	repository EventSourcing
+
+	typeStore string
+}
